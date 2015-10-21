5b80651076b29354156a39f7dd750aff8257f45d
Yuan Jiang <jiangyuan916@gmail.com>
2015-10-21 18:10:42 -0400

    video 2

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-var app = angular.module('flapperNews', []);
+var module = angular.module('flapperNews', []);
 
-app.controller('MainCtrl', [
+module.controller('MainCtrl', [
 '$scope',
 function($scope){
+  $scope.posts = [
+    {title: 'post 1', upvotes: 5},
+    {title: 'post 2', upvotes: 2},
+    {title: 'post 3', upvotes: 15},
+    {title: 'post 4', upvotes: 9},
+    {title: 'post 5', upvotes: 4}
+  ];
   $scope.test = 'Hello world!';
 }]);
