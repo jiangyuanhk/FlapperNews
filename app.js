7d791eef3f6e222b811031d5c31db71b8cf94e8d
Yuan Jiang <jiangyuan916@gmail.com>
2015-10-22 11:35:58 -0400

    inject service

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var app = angular.module('flapperNews', []);
-app.factory('psots', [function(){
+app.factory('posts', [function(){
   //service body
   var obj = {
     posts: []
@@ -8,14 +8,9 @@ app.factory('psots', [function(){
 }]);
 app.controller('MainCtrl', [
 '$scope',
-function($scope){
-  $scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}
-  ];
+'posts',
+function($scope, posts){
+  $scope.posts = posts.posts;
   $scope.test = 'Hello world!';
 
   $scope.addPost = function() {
